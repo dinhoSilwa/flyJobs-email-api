622c3d8702ed7d9538452a93cee8f5b142804910
claudio silva <claudiosilva.one@gmail.com>
2025-05-17 20:56:52 -0300

    feat : add-users-types

---
+++ src/@types/IUserService.ts
@@ -0,0 +1,11 @@
+export interface IUserService {
+  name: string;
+  email: string;
+  message: string;
+  service: string;
+  phone: string;
+  mode: string;
+  selectedServiceType: string;
+  isExpressService: boolean;
+  termsAccepted: boolean;
+}
