33b9c95721a74fbbea764df3e6742265c713397d
claudio silva <claudiosilva.one@gmail.com>
2025-05-18 14:42:41 -0300

    refact: improve-error-handling-middleware-accuracy

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,47 +1,20 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
-import { CustomApiError, NotFound, NotFoundNodemailer } from "../erros/customsErrorsApi";
-interface ErrorHandlerMiddlewareType {
-  error: Error & CustomApiError;
-  req: Request;
-  res: Response;
-  message: string;
-  next: NextFunction;
-}
-export const ErrorHandlerMiddleware: ErrorRequestHandler = ({
-  error,
-  req,
-  res,
-  message,
-  next,
-}: ErrorHandlerMiddlewareType): any => {
-  const statusCode = error.statusCode ? error.statusCode : 500;
-  message: error.message ? error.message : "Internal Server Error";
+import { CustomApiError } from "../erros/customsErrorsApi";
 
-  if (error instanceof NotFound) {
-    return res.status(error.statusCode).json({
-      error: {
-        status: statusCode,
-        message: message,
-      },
-    });
-  }
-
-  if(error instanceof NotFoundNodemailer){
-    return res.status(error.statusCode).json({
-      error : {
-        status : statusCode,
-        message : message
-      }
-    })
-  }
-
-  return res.status(statusCode).json({
+export const ErrorHandlerMiddleware: ErrorRequestHandler = (
+  error: Error & CustomApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode || 500;
+  const response = {
+    success: false,
     error: {
-      statusCode: statusCode,
-      message: message,
-      ...(process.env.Node_env === "development" && {
-        stack: error.stack,
-      }),
+      code: statusCode,
+      message: error.message || "Erro interno",
+      details: process.env.NODE_ENV === "development" ? error.stack : null,
     },
-  });
+  };
+  res.status(statusCode).json(response);
 };
