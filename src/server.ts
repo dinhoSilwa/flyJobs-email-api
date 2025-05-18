9ad8fd0e9d1d1a2c068e456af9754e3f1ff47f4d
claudio silva <claudiosilva.one@gmail.com>
2025-05-18 15:18:52 -0300

    chore : render-deploy-setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import { app } from "./app";
-
+import { config } from "dotenv";
+import { app } from "../src/app";
 const { PORT } = process.env;
+config();
 export const startServer = () => {
-  app.listen(3000, () => {
-    console.log(`Servidor Rodando na Porta ${PORT}`);
-  });
+  try {
+    app.listen(3000, () => {
+      console.log(`Servidor Rodando na Porta ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Erro ao iniciar o servidor:", err);
+  }
 };
 startServer();
