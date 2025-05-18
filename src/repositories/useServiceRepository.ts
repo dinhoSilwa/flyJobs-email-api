2ae5b4782e2d5f96f2b385ce15fd4e60d02b64e4
claudio silva <claudiosilva.one@gmail.com>
2025-05-18 13:34:39 -0300

    feat: create-service-request-email-repository

---
+++ src/repositories/useServiceRepository.ts
@@ -0,0 +1,14 @@
+import { IUserService } from "../@types/IUserService";
+import { IUserServiceRepository } from "../interfaces/IUserServiceRepository";
+import { createHtmlEmailTemplate } from "../emails/createHtmlEmailTemplate";
+import { nodemailerTransporter } from "../emails/nodemailer/nodemailerTransporter";
+
+export class UseServiceRepository
+  implements IUserServiceRepository<IUserService>
+{
+  async sendEmailService(serviceEmail: IUserService): Promise<any | Error> {
+    return await nodemailerTransporter.sendMail(
+      createHtmlEmailTemplate(serviceEmail)
+    );
+  }
+}
