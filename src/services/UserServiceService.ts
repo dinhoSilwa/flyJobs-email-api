4f538957d79a30c2e024f8f7030a5f2fd6093500
claudio silva <claudiosilva.one@gmail.com>
2025-05-18 13:30:10 -0300

    feat: create-nodemailer-service-for-email-submissions

--- src/services/UserServiceService.ts
+++ src/services/UserServiceService.ts
@@ -7,12 +7,7 @@ export class UserService {
     this.serviceRepository = serviceRepository;
   }
 
-  public async sendServiceEmail(service: IUserService): Promise<void> {
-    try {
-      await this.serviceRepository.sendEmailService(service);
-    } catch (err) {
-      console.error("Falha ao enviar Email");
-      throw new Error("Falha ao Usar O Nodemailer");
-    }
+  public async sendServiceEmail(service: IUserService): Promise<void | Error> {
+     await this.serviceRepository.sendEmailService(service);
   }
 }
