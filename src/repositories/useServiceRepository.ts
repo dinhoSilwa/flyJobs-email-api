138eac7822aeebfb1c6f47a25be1c3b3ae1b6678
claudio silva <claudiosilva.one@gmail.com>
2025-05-18 14:50:12 -0300

    feat: add-custom-error-handling-for-email-sending-failures

--- src/repositories/useServiceRepository.ts
+++ src/repositories/useServiceRepository.ts
@@ -2,10 +2,18 @@ import { IUserService } from "../@types/IUserService";
 import { IUserServiceRepository } from "../interfaces/IUserServiceRepository";
 import { createHtmlEmailTemplate } from "../emails/createHtmlEmailTemplate";
 import { nodemailerTransporter } from "../emails/nodemailer/nodemailerTransporter";
+import { NotFoundNodemailer } from "../erros/customsErrorsApi";
+
 export class UseServiceRepository
   implements IUserServiceRepository<IUserService>
 {
   async sendEmailService(serviceEmail: IUserService): Promise<any> {
-    await nodemailerTransporter.sendMail(createHtmlEmailTemplate(serviceEmail));
+    try {
+      await nodemailerTransporter.sendMail(
+        createHtmlEmailTemplate(serviceEmail)
+      );
+    } catch (err) {
+      throw new NotFoundNodemailer("Falha No Serviço do Nodemailer");
+    }
   }
 }
