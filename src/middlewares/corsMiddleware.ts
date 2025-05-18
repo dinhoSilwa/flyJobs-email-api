07c284fd6d23d7e9c0f3e4a8c1836ae7c18f9525
claudio silva <claudiosilva.one@gmail.com>
2025-05-18 17:39:44 -0300

    fix(api/cors): refactor cors config for multi-origin security

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,8 +1,24 @@
 import { CorsOptions } from "cors";
-import { config } from "dotenv";
-config();
+
+const allowedOrigins = [
+  "https://flyjobs-rh-consulting.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
+
 export const corsOptions: CorsOptions = {
-  origin: "*",
-  methods: ["POST", "GET"],
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      const errorMsg = `A origem '${origin}' não é permitida pelo CORS`;
+      return callback(new Error(errorMsg), false);
+    }
+  },
+  methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
