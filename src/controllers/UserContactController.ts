6996fe28e43fe1c2e8965c92c49e8f8cc60e9620
claudio silva <claudiosilva.one@gmail.com>
2025-05-23 17:46:21 -0300

    feat: create-contact-request-controller

---
+++ src/controllers/UserContactController.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { UserContactService } from "../services/UserContactService";
+import { ContactServiceRepository } from "../repositories/useContactRepository";
+const contactRepository = new ContactServiceRepository();
+const contact = new UserContactService(contactRepository);
+
+export class UserContactController {
+  static async startSendContactEmail(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    await contact.senContactEmail(req.body);
+    res
+      .status(200)
+      .json({ status: "OK", msg: "Email de Contato Enviado com Sucesso" });
+  }
+}
