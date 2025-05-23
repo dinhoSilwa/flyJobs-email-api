542acc648ecd1318d3a2337248f5c2773d11093c
claudio silva <claudiosilva.one@gmail.com>
2025-05-23 17:34:57 -0300

    refact: add-error-object-for-better-error-handling

--- src/utils/zodFormatError.ts
+++ src/utils/zodFormatError.ts
@@ -1,6 +1,6 @@
-import { z, ZodIssue } from "zod";
-export const zodFormatError = (issue: ZodIssue[]): any[] => {
-  const errorFormatter: [] = [];
+import { ZodIssue } from "zod";
+export const zodFormatError = (issue: ZodIssue[]): any => {
+  const errorFormatter: Record<string, string> = {};
   issue.forEach((erros) => {
     const errorFields = erros.path.join(".");
     errorFormatter[errorFields] = erros.message;
