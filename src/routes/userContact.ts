d57658b1823d614ad13a19659edcb11ebd5567c3
claudio silva <claudiosilva.one@gmail.com>
2025-05-23 18:10:49 -0300

    feat: add-contact-email-send-route

---
+++ src/routes/userContact.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { zodMiddleware } from "../middlewares/validateMiddleware";
+import { userContactSchema } from "../schema/UserContact";
+import { UserContactController } from "../controllers/UserContactController";
+
+export const UserContactRouter = Router();
+
+UserContactRouter.post(
+  "/send/contact",
+  zodMiddleware(userContactSchema),
+  UserContactController.startSendContactEmail
+);
