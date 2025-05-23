b387953ace9fea7b8b6528b010b54b9e118fb099
claudio silva <claudiosilva.one@gmail.com>
2025-05-23 18:02:53 -0300

    feat: create-zod-validation-middleware

---
+++ src/middlewares/validateMiddleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+import { zodFormatError } from "../utils/zodFormatError";
+import { ZodErrors } from "../erros/customsErrorsApi";
+
+export const zodMiddleware = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      throw new ZodErrors(zodFormatError(result.error.issues));
+    }
+    req.validate = result.data;
+    next();
+  };
+};
