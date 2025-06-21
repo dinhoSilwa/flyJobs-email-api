e326b7d7e6fa3c5291dfbf04d79f15f5bab716c0
claudio silva <claudiosilva.one@gmail.com>
2025-06-20 22:57:02 -0300

    feat: add-authentication-repository-and-interface

---
+++ src/interfaces/IUserAuth.ts
@@ -0,0 +1,7 @@
+import { IUserAuthLogin, IUserAuthSignup } from "../@types/IUserAuth";
+
+type AuthType = IUserAuthSignup | IUserAuthLogin;
+export interface IUseAuthRepository<AuthType> {
+  signup(auth: IUserAuthSignup): Promise<void>;
+  login(auth: IUserAuthLogin): Promise<void>;
+}
---
+++ src/repositories/UseAuthRepository.ts
@@ -0,0 +1,54 @@
+import { IUserAuthLogin, IUserAuthSignup } from "../@types/IUserAuth";
+import { prisma } from "../db/prisma";
+import { IUseAuthRepository } from "../interfaces/IUserAuth";
+import { HashPasswordManager } from "../utils/encrypt";
+import { AuthError } from "../errors/customsErrorsApi";
+import { TokenManager } from "../utils/TokenManager";
+
+export class UseAuthRepository implements IUseAuthRepository<IUserAuthSignup> {
+  async signup(authData: IUserAuthSignup): Promise<void> {
+    try {
+      const { password } = authData;
+      const createHashPassword = await HashPasswordManager.create(password);
+      const authDataWithHashPassword = {
+        ...authData,
+        password: createHashPassword,
+      };
+
+      await prisma.userAuth.create({
+        data: {
+          ...authDataWithHashPassword,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new AuthError("Credenciais incorretas");
+    }
+  }
+  async login(auth: IUserAuthLogin): Promise<any> {
+    const { password, email } = auth;
+    const isExistingUser = prisma.userAuth.findUnique({
+      where: { email },
+    });
+    if (!isExistingUser) return null;
+
+    const isMatchPasswordHash = await HashPasswordManager.compare(
+      password,
+      (await isExistingUser).password,
+    );
+
+    if (!isMatchPasswordHash) {
+      throw new AuthError("Credenciais inválidas");
+    }
+
+    const tokenManager = TokenManager.getInstance();
+    const token = await tokenManager.generateToken({
+      id: (await isExistingUser).id,
+      email: auth.email,
+      role: "Admin",
+    });
+    return token;
+  }
+}
